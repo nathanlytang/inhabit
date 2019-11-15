395cc5312ca81cd00167de9b18923703ea12220b
Nathan Tang <natt888@yahoo.ca>
2019-11-15 03:11:58 -0700

    Working on window buttons

---
+++ main.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow} = require('electron')
+const url = require('url')
+const path = require('path')
+
+let win
+
+function createWindow() {
+//    win = new BrowserWindow({width: 400, height: 800, })
+   win = new BrowserWindow({width: 400, height: 800, frame: false})
+//    win.setMenuBarVisibility(false)
+   win.loadURL(url.format ({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+   }))
+}
+
+app.on('ready', createWindow)
