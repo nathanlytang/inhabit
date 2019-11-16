bf8d3fb0abd9086f6ddd010fad871eec59b5144b
Nathan Tang <natt888@yahoo.ca>
2019-11-16 03:21:29 -0700

    Transparent and frameless

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ let win
 
 function createWindow() {
 //    win = new BrowserWindow({width: 400, height: 800, })
-   win = new BrowserWindow({width: 300, height: 600, frame: false})
+   win = new BrowserWindow({
+       width: 300, 
+       height: 600, 
+       frame: false, 
+       transparent: true,
+       fullscreen: false,
+       webPreferences: {
+           devTools: false
+       }
+    })
+    win.setMenu(null)
 //    win.setMenuBarVisibility(false)
    win.loadURL(url.format ({
       pathname: path.join(__dirname, 'index.html'),
