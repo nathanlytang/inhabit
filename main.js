95617a2637a669431570a529c2256d0718d6d99a
Brenden Crowie <brendencrowie@gmail.com>
2020-05-25 11:36:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require("electron");
 const url = require("url");
 const path = require("path");
+require("electron-reload")(__dirname);
 let win;
 
 function createWindow() {
