33e5a8592a5dd26a815dcbea562c6bca05b2e5b4
Nathan <nathan.ly.tang@gmail.com>
2020-08-13 02:59:35 -0600

    New folder structure

--- calendar.js
+++
@@ -1,7 +0,0 @@
-var calendar = new HelloWeek({
-    daysSelected: ['2019-04-25', '2019-05-01', '2019-05-02', '2019-05-03'],
-    multiplePick: true,
-    onSelect: () => {
-        console.log(calendar.getDays());
-    }
-});
